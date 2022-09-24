827b4cdd6803a19fa422b94c209a997f5180fcd3
ismailalahmad <asmayllalahmad90@gmail.com>
2022-09-24 18:07:02 +0300

    empty project

---
+++ LibraryManagement1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LibraryManagement1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
